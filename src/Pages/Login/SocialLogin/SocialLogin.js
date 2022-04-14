88da3d97f704a67299b63766ec99c3883d5b76c4
frsiam <siam.frs@gmail.com>
2022-04-14 14:43:24 +0600

    google sign in implement done

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import google from '../../../images/google_logo.png'
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate()
+
+    if (error) {
+        let Error = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+    }
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (user) {
+        navigate('/home')
+        // return (
+        //   <div>
+        //     <p>Signed In User: {user.email}</p>
+        //   </div>
+        // );
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
-                <div style={{height: '1px'}} className='bg-secondary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-secondary w-50'></div>
                 <p className='mt-2 px-2'>or</p>
-                <div style={{height: '1px'}} className='bg-secondary w-50'></div>
+                <div style={{ height: '1px' }} className='bg-secondary w-50'></div>
             </div>
+            {Error}
             <div>
-                <button className='btn btn-light w-75 d-block mx-auto border border-dark my-2'>
-                    <img style={{width: '30px'}} className='bg-dark' src={google} alt="" />
+                <button onClick={() => signInWithGoogle()} className='btn btn-light w-75 d-block mx-auto border border-dark my-2'>
+                    <img style={{ width: '30px' }} className='bg-dark' src={google} alt="" />
                     Google Sign In
                 </button>
                 <button className='btn btn-primary w-75 d-block mx-auto my-2'>
