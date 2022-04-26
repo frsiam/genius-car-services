307aa57dc1794ad0e50b3e4ccd9d9bca90721026
frsiam <siam.frs@gmail.com>
2022-04-27 00:15:44 +0600

    ordered product show

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:4000/orders?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+    }, [user])
     return (
-        <div>
-            <h3>Your orders summary</h3>
+        <div className='container text-center my-5'>
+            <h3>Your orders summary: {orders.length}</h3>
         </div>
     );
 };
