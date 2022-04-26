ac171a3aefef0055c11ffe0f7d944f2e66d5c070
frsiam <siam.frs@gmail.com>
2022-04-26 15:52:36 +0600

    some change

--- src/Pages/CheckOut/Checkout/Checkout.js
+++ src/Pages/CheckOut/Checkout/Checkout.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const Checkout = () => {
+    const { serviceId } = useParams()
+    const [service] = useServiceDetail(serviceId)
     return (
-        <div>
-            <h1>Please checkout your product</h1>
+        <div className='w-50 mx-auto my-5'>
+            <h1>Please order: {service.name}</h1>
+            <form>
+                
+            </form>
         </div>
     );
 };
